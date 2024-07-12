4ad694c2de410dc8a388b151c9c18d1217199dac
b-sai <bsaishreyas@gmail.com>
2024-07-11 19:48:51 -0500

    Add tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import ResumeParser from "resume-parser/page";
 import JobSearchCard from "job-board/page";
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <html>
       <body>
         <ResumeParser />
         <JobSearchCard />
+        <Analytics />
       </body>
     </html>
   );
