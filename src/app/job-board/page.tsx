637f53371c526b1dbb77625f2ffca969f74dbc0e
b-sai <bsaishreyas@gmail.com>
2024-09-03 18:15:32 -0500

    add filter search param

--- src/app/job-board/page.tsx
+++ src/app/job-board/page.tsx
@@ -427,7 +427,7 @@ const JobSearchCard: React.FC = () => {
             ))
           )}
 
-          {showTopCompanies && jobs.length > 0 && !isLoading && (
+          {showTopCompanies && jobs.length >= 0 && !isLoading && (
             <div className="p-4 text-center">
               <button
                 onClick={handleExpandSearch}
